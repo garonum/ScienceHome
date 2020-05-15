c92f3a7447b9218245688b5334bc783751b30e61
Vlad <garonum@gmail.com>
2020-05-15 22:08:01 +0300

    Initial Commit

---
+++ RAPI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RAPI
+//
+//  Created by LA'D on 15.05.2020.
+//  Copyright © 2020 Garonum. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
